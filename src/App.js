4b0848640d04e3bd4e0799a03fbc54ad70b1f88a
erosbalto <erosbaltodanog@gmail.com>
2021-02-11 13:14:13 -0600

    update panel

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import EntityList from "./components/EntityList/EntityList";
 import { Stage } from 'react-konva';
 
 function App() {
+  const width = 1920, height = 1080;
 
   return (
     <div className="App">
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Stage width={width} height={height}>
         <EntityList />
       </Stage>
     </div>
